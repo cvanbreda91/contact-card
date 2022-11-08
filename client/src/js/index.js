3f3272e55cd9b1c164f58c2153cd54554ca7df2f
Courtney Van Breda <cmvanbreda91@gmail.com>
2022-11-07 19:07:14 -0500

    add put request

--- client/src/js/index.js
+++ client/src/js/index.js
@@ -2,7 +2,7 @@ import "./submit";
 import { toggleForm, clearForm } from './form';
 import { Tooltip, Toast, Popover } from 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { initdb, getDb, postDb } from './database';
+import { initdb, getDb, postDb, deleteDb, editDb } from './database';
 import { fetchCards } from './cards';
 
 import Logo from '../images/logo.png';
@@ -17,41 +17,69 @@ window.addEventListener('load', function () {
   document.getElementById('dogThumbnail').src = Dog;
 });
 
-  // Form functionality
-  const form = document.getElementById("formToggle");
-  const newContactButton = document.getElementById("new-contact");
-  let submitBtnToUpdate = false;
-  let profileId;
-  
-  newContactButton.addEventListener('click', event => {
-    toggleForm()
-   })
-  
-  form.addEventListener('submit', event => {
-    // Handle data
-    event.preventDefault();
+// Form functionality
+const form = document.getElementById("formToggle");
+const newContactButton = document.getElementById("new-contact");
+let submitBtnToUpdate = false;
+let profileId;
+
+newContactButton.addEventListener('click', event => {
+  toggleForm()
+})
+
+form.addEventListener('submit', event => {
+  // Handle data
+  event.preventDefault();
   let name = document.getElementById("name").value;
   let phone = document.getElementById("phone").value;
   let email = document.getElementById("email").value;
   let profile = document.querySelector('input[type="radio"]:checked').value;
-  
-    // Post form data to IndexedDB OR Edit an existing card in IndexedDB
+
+  // Post form data to IndexedDB OR Edit an existing card in IndexedDB
   if (submitBtnToUpdate == false) {
     postDb(name, email, phone, profile);
   } else {
-  
+
     fetchCards();
-      // Toggles the submit button back to POST functionality
+    // Toggles the submit button back to POST functionality
     submitBtnToUpdate = false;
   }
-  
+
   // Clear form
   clearForm();
   // Toggle form
   toggleForm();
   // Reload the DOM
   fetchCards();
-  });
+});
+
+window.deleteCard = (e) => {
+  // Grabs the id from the button element attached to the contact card.
+  let id = parseInt(e.id);
+  // Delete the card
+  deleteDb(id);
+  // Reload the DOM
+  fetchCards();
+};
+
+window.editCard = (e) => {
+  // Grabs the id from the button element attached to the contact card and sets a global variable that will be used in the form element.
+  profileId = parseInt(e.dataset.id);
+
+  // Grabs information to pre-populate edit form
+  let editName = e.dataset.name;
+  let editEmail = e.dataset.email;
+  let editPhone = e.dataset.phone;
+
+  document.getElementById("name").value = editName;
+  document.getElementById("email").value = editEmail;
+  document.getElementById("phone").value = editPhone;
+
+  form.style.display = "block";
+
+  // Toggles the Submit button so that it now Updates an existing contact instead of posting a new one
+    submitBtnToUpdate = true;
+};
 
 // Import CSS files
 import "../css/index.css";
